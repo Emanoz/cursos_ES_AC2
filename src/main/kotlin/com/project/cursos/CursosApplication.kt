2199c122e8657d3df075e030d2a5aaa77e27c43e
Emanuel Lourençon <emanuel.lourencon@hotmail.com>
2022-05-08 22:21:47 -0300

    Ajustes

--- src/main/kotlin/com/project/cursos/CursosApplication.kt
+++ src/main/kotlin/com/project/cursos/CursosApplication.kt
@@ -1,24 +1,28 @@
 package com.project.cursos
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
+
 @Controller
 @SpringBootApplication
 @EnableSwagger2
-class CursosApplication {
+class PraticandoApiApplication {
+	@RequestMapping("/")
+	@ResponseBody
+	fun home(): String {
+		return "index.html"
+	}
 
-	fun main(args: Array<String>) {
-		runApplication<CursosApplication>(*args)
-
-		@RequestMapping("/")
-		@ResponseBody
-		fun home(): String? {
-			return "index.html"
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			SpringApplication.run(PraticandoApiApplication::class.java, *args)
 		}
 	}
 }
+
